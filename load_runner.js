ddbd042415d399312a6aab9969f0b0aed323c6c3
John Ford <john@johnford.org>
2019-02-25 17:58:31 +0100

    Change how parrallelism works for load generator

--- load_runner.js
+++ load_runner.js
@@ -40,43 +40,43 @@ async function test() {
 
   while (true) {
     let start = process.hrtime.bigint();
-    for (let i = 0; i < n; i++) {
-      let id = uuid.v4();
+    let id = uuid.v4();
 
-      await request({
-        id,
-        method: 'PUT',
-        body: JSON.stringify({
-          todoId: id,
-          priority: Math.floor(Math.random() * 10) + 1,
-          title: 'clean kitchen',
-          body: 'make sure the kitchen is clean',
-          due: new Date(),
-        }),
-      });
+    await request({
+      id,
+      method: 'PUT',
+      body: JSON.stringify({
+        todoId: id,
+        priority: Math.floor(Math.random() * 10) + 1,
+        title: 'clean kitchen',
+        body: 'make sure the kitchen is clean',
+        due: new Date(),
+        completed: false,
+      }),
+    });
 
-      await request({id});
-      await request({id});
+    await request({id});
+    await request({id});
 
-      await request({
-        id,
-        method: 'POST',
-        body: JSON.stringify({
-          todoId: id,
-          priority: Math.floor(Math.random() * 10) + 1,
-          title: 'dirty kitchen',
-          body: 'make sure the kitchen is dirty',
-          due: new Date(),
-        }),
-      });
+    await request({
+      id,
+      method: 'POST',
+      body: JSON.stringify({
+        todoId: id,
+        priority: Math.floor(Math.random() * 10) + 1,
+        title: 'dirty kitchen',
+        body: 'make sure the kitchen is dirty',
+        due: new Date(),
+        completed: true,
+      }),
+    });
 
-      await request({id});
-      await request({id});
-      await request({id});
+    await request({id});
+    await request({id});
+    await request({id});
 
-      if (Math.random() < 0.1) {
-        await request({id, method: 'DELETE'});
-      }
+    if (Math.random() < 0.1) {
+      await request({id, method: 'DELETE'});
     }
     let end = process.hrtime.bigint();
     let duration = Number(end - start) / 1e9;
@@ -98,5 +98,10 @@ if (cluster.isMaster) {
     cluster.fork();
   });
 } else {
-  test().then(console.log, console.error);
+  Promise.race([
+    test(),
+    test(),
+    test(),
+    test(),
+  ]).catch(console.error);
 }
